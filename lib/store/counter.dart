ff7784c5ca44268235ef3c69f2cd6497ed97d438
albertrein <guilhermealberto8@gmail.com>
2022-01-19 04:54:49 +0000

    Starting mobx with simple counter.

---
+++ lib/store/counter.dart
@@ -0,0 +1,14 @@
+import 'package:mobx/mobx.dart';
+part 'counter.g.dart';
+
+class CounterStore = _CounterStore with _$CounterStore;
+
+abstract class _CounterStore with Store{
+  @observable
+  int counter = 0;
+
+  @action
+  void incrementCounter(){
+    counter++;
+  }
+}
